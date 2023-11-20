c9b34c76b5e4ea53d4243f2e209d4ca9c8ea4a4c
Willian Dutra <contatowilliandutra@gmail.com>
2023-11-20 11:44:33 -0300

    Arrumando docker e configuracao do sequelize

--- back-end/src/config/config.js
+++ back-end/src/config/config.js
@@ -9,12 +9,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME}${suffix[environment]}`,
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+    `${process.env.MYSQL_DB_NAME || 'portfolio'}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '123456',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
