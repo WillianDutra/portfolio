d486e7fdd11b45c137696d488df8d6af683f6bf3
Willian Dutra <contatowilliandutra@gmail.com>
2023-11-22 13:16:29 -0300

    Adicionando endpoint para verificar banco de dados e criar novo usuario no arquivo de rotas

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
-// const {} = require('../controllers');
-// const {} = require('../middlewares');
-
 const apiRoutes = express.Router();
+const { sequelize } = require('../models');
+const { User } = require('../controllers');
+const { validateNewUser } = require('../middlewares');
 
-apiRoutes.get('/', (_req, res) => {
-  res.send('Hello World!');
+apiRoutes.get('/health', async (_req, res) => {
+  try {
+    await sequelize.authenticate();
+    return res.status(200).json({ message: 'Connection has been established successfully.' });
+  } catch (error) {
+    return res.status(500).json({ message: `Unable to connect to the database: ${error.message}` });
+  }
 });
+
+apiRoutes.post('/user', validateNewUser, User.createUser);
 
 module.exports = apiRoutes;
